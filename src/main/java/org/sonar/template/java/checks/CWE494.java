54a00a6701310d04aa0d98e11005d37ac98ef934
asier <asier@DESKTOP-O7HQNG2>
2017-01-17 17:06:43 +0100

    s

--- src/main/java/org/sonar/template/java/checks/CWE494.java
+++ src/main/java/org/sonar/template/java/checks/CWE494.java
@@ -1,45 +1,57 @@
 package org.sonar.template.java.checks;
- 
+
 import com.google.common.collect.ImmutableList;
 
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.tree.ClassTree;
 import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.Tree.Kind;
- 
+import org.sonar.plugins.java.api.tree.VariableTree;
+
+import java.util.ArrayList;
 import java.util.List;
- 
-@Rule(
-  key = "CWE-494",
-  name = "Download of Code Without Integrity Check",
-  description = "",
-  priority = Priority.MAJOR,
-  tags = {"sans-top25-risky"})
+
+@Rule(key = "CWE-494", name = "Download of Code Without Integrity Check", description = "", priority = Priority.MAJOR, tags = {
+		"sans-top25-risky" })
 
 public class CWE494 extends IssuableSubscriptionVisitor {
- 
-  @Override
-  public List<Kind> nodesToVisit() {
-    return ImmutableList.of(Kind.METHOD_INVOCATION);
-  }
-  
-  @Override
-  public void visitNode(Tree tree) {
-	  MethodInvocationTree lt = (MethodInvocationTree) tree;
-	  if(lt.methodSelect().is(Kind.MEMBER_SELECT)){
-		  MemberSelectExpressionTree ms = (MemberSelectExpressionTree) lt.methodSelect();
-		  if(ms.firstToken().text().equals("encer")){
-			  if(ms.identifier().name().equals("update")){
-				  if(!lt.arguments().get(0).kind().toString().equals("PLUS")){
-					  reportIssue(lt, "You need to add salt");
-				  }
-			  }
-		  }
-	  }
 
-  }
- 
+	private ArrayList<String> MessageDigestVariables;
+
+	public CWE494() {
+		MessageDigestVariables = new ArrayList<>();
+	}
+
+	@Override
+	public List<Kind> nodesToVisit() {
+		return ImmutableList.of(Kind.VARIABLE, Kind.METHOD_INVOCATION);
+	}
+
+	@Override
+	public void visitNode(Tree tree) {
+		if (tree instanceof VariableTree) {
+			VariableTree vt = (VariableTree) tree;
+			if (vt.type().toString().equals("MessageDigest")) {
+				MessageDigestVariables.add(vt.simpleName().toString());
+			}
+		} else if (tree instanceof MethodInvocationTree) {
+			MethodInvocationTree mit = (MethodInvocationTree) tree;
+			if (mit.methodSelect().is(Kind.MEMBER_SELECT)) {
+				MemberSelectExpressionTree ms = (MemberSelectExpressionTree) mit.methodSelect();
+				if (MessageDigestVariables.contains(ms.firstToken().text())) {
+					if (ms.identifier().name().equals("update")) {
+						if (!mit.arguments().get(0).kind().toString().equals("PLUS")) {
+							reportIssue(mit, "You need to add salt");
+						}
+					}
+				}
+			}
+		}
+
+	}
+
 }
--- src/test/files/CWE494testFile.java
+++ src/test/files/CWE494testFile.java
@@ -14,7 +14,7 @@ class MyClass {
   void loginWithSalt1(String user, String pass){
 	  String salt = getSalt(user);
 	  MessageDigest encer = MessageDigest.getInstance("SHA");  
-	  encer.update(pass+salt);
+	  encer.update(pass+salt); 
 	  byte[] digest = encer.digest();
 	  if (equal(digest,secret_password(user))) {
 		  login_user();
